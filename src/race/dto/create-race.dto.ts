b344871c0a848b1797bd3c3ae0c5530a0a41a765
GuilhermeCardosoSantos <guifoxlokaum@gmail.com>
2024-04-02 15:03:32 -0300

    FIX: update

--- src/race/dto/create-race.dto.ts
+++ src/race/dto/create-race.dto.ts
@@ -1,6 +1,5 @@
 export class CreateRaceDto {
     idClient: number;
-    idDriver?: number;
     km: number;
     initial: string;
     finish: string;
--- src/race/dto/update-race.dto.ts
+++ src/race/dto/update-race.dto.ts
@@ -1,3 +1,6 @@
 export class UpdateRaceDto {
     isVisible? : string
+    idDriver?: number;
+    dalete_at?:string;
+    value?: string;
 }
