60f5d02b51e3e6b4ba64d7b54a33677ae3ac75f4
matheusbanqueiro <mbanqueirolima@gmail.com>
2024-04-29 11:27:07 -0300

    FEAT: DTO and update rotes

--- src/client/client.service.ts
+++ src/client/client.service.ts
@@ -1,119 +1,141 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, ConflictException } from '@nestjs/common';
 import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
 import { Client } from './entities/client.entity';
 import { Repository } from 'typeorm';
-import Time from '../../hooks/time'
+import Time from '../../hooks/time';
 import { Sign } from './dto/sign-client.dto';
 
 @Injectable()
 export class ClientService {
   constructor(
-    @Inject('CLIENT_REPOSITORY') 
+    @Inject('CLIENT_REPOSITORY')
     private clientRepository: Repository<Client>,
-  ){}
+  ) {}
 
-  async sign(sign: Sign){
-    const email = sign.email
-    try{
+  async sign(sign: Sign) {
+    const email = sign.email;
+    try {
       const user = await this.clientRepository.findOne({
         where: { email },
       });
-      if(!user){
+      if (!user) {
         return {
           status: 401,
-          msg: 'Unauthorized'
-        }
+          msg: 'Unauthorized',
+        };
       }
-      if(user.password === sign.password){
+      if (user.password === sign.password) {
         return {
           status: 200,
-          msg: 'Authorized'
-        }
+          msg: 'Authorized',
+        };
       }
       return {
         status: 401,
-        msg: 'Unauthorized'
-      }
-    }catch(e){
-      console.log(e)
+        msg: 'Unauthorized',
+      };
+    } catch (e) {
+      console.log(e);
       return {
         status: 500,
-        msg: 'Error internal'
-      }
-    } 
+        msg: 'Error internal',
+      };
+    }
   }
 
   async create(createClientDto: CreateClientDto) {
-    const time = Time()
+    const time = Time();
     const verifyEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     createClientDto.create_at = time;
-    try{
-      const email = createClientDto.email
+    try {
+      const email = createClientDto.email;
       const existingEmail = await this.clientRepository.findOne({
         where: { email },
       });
-      if(existingEmail){
+      if (existingEmail) {
         return {
           status: 409,
-          msg: 'Email Error Already Exists'
-        }
+          msg: 'Email Error Already Exists',
+        };
       }
-      if(!verifyEmail.test(email)){
+      if (!verifyEmail.test(email)) {
         return {
           status: 409,
-          msg: 'Email Invalid'
-        }
+          msg: 'Email Invalid',
+        };
       }
-      if(createClientDto.phone.length < 11 || createClientDto.phone.length > 12){
+      if (
+        createClientDto.phone.length < 11 ||
+        createClientDto.phone.length > 12
+      ) {
         return {
           status: 409,
-          msg: 'Phone Invalid'
-        }
+          msg: 'Phone Invalid',
+        };
       }
-      if(createClientDto.password.length != 10){
+      if (createClientDto.password.length != 10) {
         return {
           status: 409,
-          msg: 'Password invalid'
-        }
+          msg: 'Password invalid',
+        };
       }
-      if(createClientDto.name.length < 4){
+      if (createClientDto.name.length < 4) {
         return {
           status: 409,
-          msg: 'Name invalid'
-        }
+          msg: 'Name invalid',
+        };
       }
       await this.clientRepository.save(createClientDto);
       return {
         status: 201,
-        msg: 'Successful registration user'
-      }
-    }catch(e){
-      console.log(e)
+        msg: 'Successful registration user',
+      };
+    } catch (e) {
+      console.log(e);
       return {
         status: 500,
-        msg: 'Error internal'
-      }
-    } 
+        msg: 'Error internal',
+      };
+    }
   }
 
   findAll() {
-    return `This action returns all client`;
+    return this.clientRepository.find();
   }
 
   findOne(email: string) {
-    return this.clientRepository.findOne({where:{email}});
+    return this.clientRepository.findOne({ where: { email } });
   }
 
   findOneById(id: number) {
-    return this.clientRepository.findOne({where:{id}});
+    return this.clientRepository.findOne({ where: { id } });
   }
 
-  update(id: number, updateClientDto: UpdateClientDto) {
-    return `This action updates a #${id} client`;
+  async update(id: number, updateClientDto: UpdateClientDto) {
+    try {
+      const allClients = await this.findAll();
+      
+      const isEmailExists = allClients.some(client => client.email === updateClientDto.email);
+      
+      if (isEmailExists) {
+        throw new ConflictException('Email already exists');
+      }
+      
+      await this.clientRepository.update(id, updateClientDto);
+      return {
+        status: 201,
+        msg: 'Successful Data Updated',
+      };
+    } catch (e) {
+      return {
+        status: 500,
+        msg: 'Error server internal',
+      };
+    }
   }
 
   async remove(id: number) {
-    return id
+    return id;
   }
 }
--- src/client/dto/sign-client.dto.ts
+++ src/client/dto/sign-client.dto.ts
@@ -1,4 +1,4 @@
 export class Sign {
-    email:string;
-    password:string;
+  email: string;
+  password: string;
 }
