befed2fee6af81456f051852d6767a437e6dde69
GuilhermeCardosoSantos <guifoxlokaum@gmail.com>
2024-05-15 16:43:14 -0300

    FIX: date

--- hooks/time.ts
+++ hooks/time.ts
@@ -1,4 +1,5 @@
 export default function findTimeSP(){
     const agora = new Date();
-    return agora.toISOString();
+    const horarioBrasilia = new Date(agora.getTime() - (3 * 60 * 60 * 1000));
+    return horarioBrasilia.toISOString();
 }
