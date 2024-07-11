c71762322925530376f45e8ed1274f2f13248641
GuilhermeCardosoSantos <guifoxlokaum@gmail.com>
2024-07-11 14:06:21 -0300

    FEAT: remove whats

--- src/whats/dto/update-what.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateWhatDto } from './create-what.dto';
-
-export class UpdateWhatDto extends PartialType(CreateWhatDto) {}
--- src/whats/whats.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { WhatsService } from './whats.service';
-import { WhatsController } from './whats.controller';
-
-@Module({
-  controllers: [WhatsController],
-  providers: [WhatsService],
-})
-export class WhatsModule {}
